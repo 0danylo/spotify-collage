3563155d8b4efda62b98ee2cb282e5b6e14b426d
Danylo <danylovoloshyn0@gmail.com>
2024-03-24 19:49:56 -0400

    vercel structure

--- api/index.js
+++ api/index.js
@@ -5,11 +5,12 @@ const querystring = require('querystring')
 const cookieParser = require('cookie-parser')
 
 const clientID = 'b73bb6d645664a5c8648994b4b95a763'
-const clientSecret = require('./client-secret.js')
-const redirectURI = 'http://spotify-collage.vercel.app/callback'
+// let clientSecret = require('./client-secret.js')
+const redirectURI = 'http://localhost:3000/callback'
 
 const stateKey = 'spotify_auth_state';
 const app = express();
+
 
 const generateRandomString = length => {
     let text = '';
@@ -19,13 +20,16 @@ const generateRandomString = length => {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
-};
+};  
 
-app.use(express.static(__dirname + '/public'))
+console.log('clientID:', clientID)
+
+app.use(express.static(__dirname + '../public'))
     .use(cors())
     .use(cookieParser());
 
 app.get('/login', (req, res) => {
+    console.log('login')
     const state = generateRandomString(16);
     res.cookie(stateKey, state);
 
@@ -41,50 +45,51 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/callback', (req, res) => {
-        const code = req.query.code || null;
-        const state = req.query.state || null;
-        const storedState = req.cookies ? req.cookies[stateKey] : null;
+    // console.log('callback')
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
 
-        if (state === null || state !== storedState) {
-            res.redirect('/#' +
-                querystring.stringify({
-                    error: 'state_mismatch'
-                }));
-        } else {
-            res.clearCookie(stateKey);
-            const authOptions = {
-                url: 'https://accounts.spotify.com/api/token',
-                form: {
-                    code: code,
-                    redirect_uri: redirectURI,
-                    grant_type: 'authorization_code'
-                },
-                headers: {
-                    'Authorization': 'Basic ' + Buffer.from(clientID + ':' + clientSecret).toString('base64')
-                },
-                json: true
-            };
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        res.clearCookie(stateKey);
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirectURI,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from(clientID + ':' + require('../client-secret.js')).toString('base64')
+            },
+            json: true
+        };
 
-            request.post(authOptions, (error, response, body) => {
-                if (!error && response.statusCode === 200) {
+        request.post(authOptions, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
 
-                    const access_token = body.access_token,
-                        refresh_token = body.refresh_token;
+                const access_token = body.access_token,
+                    refresh_token = body.refresh_token;
 
-                    res.redirect('/#' +
-                        querystring.stringify({
-                            access_token: access_token,
-                            refresh_token: refresh_token
-                        }));
-                } else {
-                    res.redirect('/#' +
-                        querystring.stringify({
-                            error: 'invalid_token'
-                        }));
-                }
-            });
-        }
-    });
+                res.redirect('/#' +
+                    querystring.stringify({
+                        access_token: access_token,
+                        refresh_token: refresh_token
+                    }));
+            } else {
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    }));
+            }
+        });
+    }
+});
 
-app.listen();
-console.log('Listening on 8888');
+app.listen(3000, () => console.log("Server ready on port 3000."));
+module.exports = app;
